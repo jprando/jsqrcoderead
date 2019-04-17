ad241eb0e08eeefbd4ffad92b0c0d66bc063207c
Jeudi Prando <jeudiprando@gmail.com>
2019-04-17 03:56:44 -0300

    realocando time e timeEnd

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fileName = path.join(__dirname, 'original.jpg')
 var qr = new QrCode()
 
 const qrcoderead = (efeito, nivel) => new Promise(async function (resolve) {
+  console.time('.')
   await Jimp.read(fileName)
     .then(image => {
       let log = 'n'
@@ -21,12 +22,12 @@ const qrcoderead = (efeito, nivel) => new Promise(async function (resolve) {
       /// image.write(path.join(__dirname, 'original.out.jpg'))
       try {
         let r = qr.process(image.bitmap).result
-        if (r) resolve(`${log} ${r}`)
+        console.timeEnd('.')
+        resolve(`${log} ${r}`)
       } catch { }
     })
 })
 console.log('...')
-console.time('.')
 Promise.race([
   new Promise((resolve, reject) => setTimeout(reject, 380, 'tempo excedido')),
   qrcoderead(),
@@ -45,7 +46,6 @@ Promise.race([
   */
   qrcoderead('g', 2)
 ])
-  .then(v => console.timeEnd('.') || v)
   .then(v => console.log('# OK', v))
   .catch(err => console.log('# ERRO', err))
   .finally(p => process.exit())
