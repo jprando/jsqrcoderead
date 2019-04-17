34b6399d75fc988efcd0a9d596c79408d85ced35
Jeudi Prando <jeudiprando@gmail.com>
2019-04-16 23:45:17 -0300

    refatoracao

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const Jimp = require('jimp')
 const fileName = path.join(__dirname, 'original.jpg')
 
 var qr = new QrCode()
+/// qrcode config
 qr.callback = function (err, value) {
   if (err) {
     console.error(err)
-    // TODO handle error
   } else {
     console.log(value.result)
     console.log(value)
@@ -18,12 +18,11 @@ qr.callback = function (err, value) {
 Jimp.read(fileName, (err, image) => {
   if (err) {
     console.error(err)
-    process.exit(1)
+  } else {
+    /// image.blur(1) // rapido e menos preciso
+    image.gaussian(1) // lento e mais preciso
+    /// opcional
+    image.write(path.join(__dirname, 'original.out.jpg'))
+    qr.decode(image.bitmap)
   }
-  // image.blur(1) // rapido e menos preciso
-  image.gaussian(1) // lento e mais preciso
-  /// opcional
-  image.write(path.join(__dirname, 'original.out.jpg'))
-
-  qr.decode(image.bitmap)
 })
