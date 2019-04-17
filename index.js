d10fc628eb011bb234bd79028a431603c6fe271e
Jeudi Prando <jeudiprando@gmail.com>
2019-04-17 03:35:47 -0300

    implementando promise race

--- index.js
+++ index.js
@@ -3,26 +3,49 @@ const QrCode = require('qrcode-reader')
 const Jimp = require('jimp')
 
 const fileName = path.join(__dirname, 'original.jpg')
-
 var qr = new QrCode()
-/// qrcode config
-qr.callback = function (err, value) {
-  if (err) {
-    console.error(err)
-  } else {
-    console.log(value.result)
-    console.log(value)
-  }
-}
 
-Jimp.read(fileName, (err, image) => {
-  if (err) {
-    console.error(err)
-  } else {
-    /// image.blur(1) // rapido e menos preciso
-    image.gaussian(1) // lento e mais preciso
-    /// opcional
-    image.write(path.join(__dirname, 'original.out.jpg'))
-    qr.decode(image.bitmap)
-  }
+const qrcoderead = async (efeito, nivel) => new Promise(function (resolve) {
+  Jimp.read(fileName)
+    .then(image => {
+      let log = 'n'
+      if (efeito && nivel) { log = efeito + nivel }
+      if (efeito === 'b') {
+        // rapido e menos preciso
+        image.blur(nivel)
+      } else if (efeito === 'g') {
+        // lento e mais preciso
+        image.gaussian(nivel)
+      }
+      /// opcional
+      /// image.write(path.join(__dirname, 'original.out.jpg'))
+      try {
+        let r = qr.process(image.bitmap).result
+        if (r) resolve(`${log} ${r}`)
+      } catch { }
+    })
 })
+console.log('...')
+console.time('.')
+Promise.race([
+  new Promise((resolve, reject) => setTimeout(reject, 370, 'tempo excedido')),
+  qrcoderead(),
+  qrcoderead('b', 1),
+  /*
+  qrcoderead('b', 1.25),
+  qrcoderead('b', 1.5),
+  qrcoderead('b', 1.75),
+  */
+  qrcoderead('b', 2),
+  qrcoderead('g', 1),
+  /*
+  qrcoderead('g', 1.25),
+  qrcoderead('g', 1.5),
+  qrcoderead('g', 1.75),
+  */
+  qrcoderead('g', 2)
+])
+  .then(v => console.timeEnd('.') || v)
+  .then(v => console.log('# OK', v))
+  .catch(err => console.log('# ERRO', err))
+  .finally(p => process.exit())
